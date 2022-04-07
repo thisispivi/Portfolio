148eed5b04d73b11c1cec64a14f9de3e2602d495
Andrea Piras <37103053+thisispivi@users.noreply.github.com>
2022-04-07 04:26:57 +0200

    Update

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function updateTime(){
+    var currentTime = new Date()
+    var hours = currentTime.getHours()
+    var minutes = currentTime.getMinutes()
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
+    var t_str = hours + ":" + minutes + " ";
+    if(hours > 11){
+        t_str += "PM";
+    } else {
+        t_str += "AM";
+    }
+    document.getElementById('time').innerHTML = t_str;
+    console.log(t_str)
+}
+
+setInterval(updateTime, 1000);
